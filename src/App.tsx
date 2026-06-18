d2494ba799dd888d4f37060c9909304bf99fc95c
bsh-ko <ssuh3872@gmail.com>
2026-06-18 12:22:06 +0900

    🔥 다크모드 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import "./App.css";
-import { useTheme } from "./hooks/useTheme";
 
 function App() {
-  const { theme, setTheme } = useTheme();
-
-  const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-bg-base text-black dark:text-white">
-      <button onClick={toggleTheme} className="items-center border px-4 py-2">
-        현재 테마: {theme} / 테마 전환
-      </button>
-    </div>
-  );
+  return <div>포폴 만들기</div>;
 }
 
 export default App;
