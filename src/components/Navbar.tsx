deca152a5e85da6f1d8e1a0acc354cedaed7b05e
bsh-ko <ssuh3872@gmail.com>
2026-06-25 16:14:05 +0900

    💄 텍스트 색상, 호버, 배치 디자인 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 px-6 py-4 bg-bg-surface">
-      <div className="flex justify-between">
-        <div>logo</div>
+    <nav className="fixed top-0 left-0 right-0 z-50 px-20 py-4 align-items bg-bg-surface/80 backdrop-blur-md border-b border-gray-200 dark:border-gray-900">
+      <div className="flex justify-between gap-4 ">
+        <div className="font-bold text-xl text-accent hover:text-accent-hover">SUHYUN</div>
         <div>
-          <ul className="flex gap-4">
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+          <ul className="flex gap-4 align-items text-gray-700 dark:text-gray-300 ">
+            <li className="hover:text-accent">About</li>
+            <li className="hover:text-accent">Projects</li>
+            <li className="hover:text-accent">Contact</li>
             <li>
               <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
                 {theme === "light" ? "🌙" : "☀️"}
