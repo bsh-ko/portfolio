fef5a64cc4107fd9eb31027e06e2a6b8c8a6914e
bsh-ko <ssuh3872@gmail.com>
2026-06-26 16:16:23 +0900

    ✨ 스킬 탭 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Layout from "./components/Layout";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Skills from "./sections/Skills";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Layout>
         <Hero />
         <About />
+        <Skills />
       </Layout>
     </ThemeProvider>
   );
