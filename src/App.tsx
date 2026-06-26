a7ba70371d6cf986750e87effc4656f39da7b907
bsh-ko <ssuh3872@gmail.com>
2026-06-26 13:31:39 +0900

    ✨ Provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import Layout from "./components/Layout";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Hero />
+        <About />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default App;
