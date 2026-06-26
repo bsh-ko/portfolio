fd5c31d5735e55b4c8bd73a9249c69fbb21bb9af
bsh-ko <ssuh3872@gmail.com>
2026-06-26 13:29:27 +0900

    💄 디자인 변경 및 테마 관련 contextAPI 연동 추가

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,31 @@
+import { useThemeContext } from "../context/ThemeContext";
 export default function Hero() {
+  const { theme } = useThemeContext();
+  console.log(theme);
+
+  const text = `
+   백엔드와 프론트엔드 양쪽을 경험하며 풀스택 역량을 쌓고 있습니다.
+   문제를 마주했을 때 포기하지 않고 끝까지 파고드는 것이 저의 가장 큰 강점입니다.
+  `;
+
   return (
-    <section className="min-h-[70vh] flex items-center justify-center px-6 md:px-16 pt-20 gap-16 bg-accent-bg dark:bg-accent-dark/20 border-b-3 border-accent/20 dark:border-accent/30">
-      <img src="/suhyun.jpg" alt="내 사진" className="w-48 h-fit rounded-full object-cover" />
+    <section
+      className="min-h-[90vh] flex items-center justify-center text-center px-6 md:px-16 pt-20 gap-16"
+      style={{
+        background:
+          theme === "dark"
+            ? "linear-gradient(135deg, #0d0b1a, #0d1117, #1a1535)"
+            : "linear-gradient(135deg, #eeedfe, #ffffff, #afa9ec)",
+        backgroundSize: "200% 200%",
+        animation: "gradient-shift 6s ease infinite",
+      }}
+    >
       <div>
-        <p className="text-gray-700 dark:text-gray-300 text-sm mb-3">안녕하세요,</p>
-        <h1 className="text-5xl dark:text-gray-300 font-bold mb-4">배수현입니다.</h1>
-        <p className="text-xl text-gray-500 dark:text-gray-400">
-          문제를 기억하고 성장하는 <span className="text-accent font-bold">풀스택 개발자</span>
-        </p>
+        <p className="text-gray-700 dark:text-gray-300 text-lg mb-3">안녕하세요,</p>
+        <h1 className="text-5xl dark:text-gray-300 font-bold mb-4">
+          <span className="text-accent font-bold">풀스택 개발자</span> 배수현입니다.
+        </h1>
+        <p className="whitespace-pre-line text-gray-500 dark:text-gray-400">{text}</p>
       </div>
     </section>
   );
